e1692d03640f902fd03577bf347e6dbd59454b2f
bdanglot <bdanglot@gmail.com>
2016-02-06 14:15:41 +0100

    continue sarsa

--- tp5/sarsa.py
+++ tp5/sarsa.py
@@ -4,7 +4,21 @@ import random
 import math
 import sys
 
-def selectAction(Q,rewards,state,explore=True):
+def printGW():
+	for y in range(sizey):
+		line = ""
+		for x in range(sizex):
+			if y*sizex+x in cliff:
+				line += "X"
+			elif y*sizex+x == start:
+				line += "S"
+			elif y*sizex+x == goal:
+				line += "G"
+			else:
+				line += " "
+		print(line)
+
+def selectAction(Q,state,explore=True,action=None):
 	maxQ=-sys.maxsize
 	selectedAction = -1
 	possibleAction = []
@@ -33,43 +47,45 @@ def selectAction(Q,rewards,state,explore=True):
 alpha=0.85
 gamma=0.05
 
-#Taile du gridworld 5x5
-sizex = 5
-sizey = 5
+#Taile du gridworld 12x4
+sizex = 12
+sizey = 4
 
 #start point and goal point
-start = 20
-goal = 24
+start = 36
+goal = 47
 
-walls = []
-walls.append(21)
-walls.append(22)
-walls.append(23)
+cliff = []
+
+for i in range (37,47,1):
+	cliff.append(i)
+
 
 print("Gridworld sarsa avec une falaise en bas du plateau")
 print("4 actions possibles : NORTH, EAST, SOUTH, WEST")
-print("Grille de taille 5x5 = 25")
-print("Initialisation de la matrice de recompenses... [25x4]")
+print("Grille de taille 12x4 = 48")
+print("Initialisation de la matrice de recompenses... [48x4]")
 rewards=np.zeros((sizex*sizey,4))
 for y in range(sizey):
 	for x in range(sizex):
 		for a in range(4):
-			if (y == 0 and a == 0) or (x == sizex-1 and a == 1) or (y == sizey-1 and a == 2) or (x == 0 and a == 3):
+			if y*sizex+x in cliff:
+				rewards[y*sizex+x][a] = -100
+			elif (y == 0 and a == 0) or (x == sizex-1 and a == 1) or (y == sizey-1 and a == 2) or (x == 0 and a == 3):
 				rewards[y*sizex+x][a] = -1
-			elif y*sizex+x in walls:
-				rewards[y*sizex+x][a] = -1			
 			else:
 				rewards[y*sizex+x][a] = 0
-for a in range(4):
-	rewards[goal][a] = 10
 
-print("Initialisation de la matrice de transitions...[25x4]")
+print("Initialisation de la matrice de transitions...[48x4]")
 transition=np.zeros((sizex*sizey,4))
 for y in range(sizey):
 	for x in range(sizex):
 		for a in range(4):
-			if (y == 0 and a == 0) or (x == sizex-1 and a == 1) or (y == sizey-1 and a == 2) or (x == 0 and a == 3):
+			if y*sizex+x in cliff:
+				transition[y*sizex+x][a] = start
+			elif (y == 0 and a == 0) or (x == sizex-1 and a == 1) or (y == sizey-1 and a == 2) or (x == 0 and a == 3):
 				transition[y*sizex+x][a] = y*sizex+x
+
 			elif a == 0:
 				transition[y*sizex+x][a] = ((y-1)*sizex)+x
 			elif a == 1:
@@ -79,31 +95,33 @@ for y in range(sizey):
 			else:
 				transition[y*sizex+x][a] = y*sizex+x-1
 
+
+
 #Init Q_0
 print("Sarsa learning")
 Q=np.zeros((sizex*sizey, 4))
 nbTurn = 1
-for _ in range(nbTurn):
+
+for _ in range(100):
 	s = start
-	a_p = selectAction(Q,rewards,s)
-	s = transition[s][a_p]
-	a = a_p
-	r_p = rewards[s][a_p]
+	a = selectAction(Q,s)
+	s = transition[s][a]
+	r = rewards[s][a]
 	while not s == goal:
-		a_p = selectAction(Q,rewards,s)
-		s_p = transition[s][a_p]
+		a_p = selectAction(Q,s)
 		r_p = rewards[s][a_p]
+		s_p = transition[s][a_p]
 		delta = r_p + gamma*Q[s_p][a_p] - Q[s][a]
-		Q[s][a] = Q[s][a] + alpha * delta
+		Q[s][a] = Q[s][a] + (alpha * delta)
 		s = s_p
 		a = a_p
 
-print(Q)
-
+print(Q[start])
+'''
 print("Solution ...")
 s = start
 while not s == goal:
-	a = selectAction(Q,rewards,s,explore=False)
+	a = selectAction(Q,s,explore=False)
 	s = transition[s][a]
-	print(a)			
+	print(a)'''		
 
